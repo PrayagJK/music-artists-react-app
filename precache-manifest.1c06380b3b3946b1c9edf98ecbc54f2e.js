f4c54ea92f27fd51e25f6850b8727012e40a5a79
Prayag Kasture <prayagkasture@Prayags-MacBook-Air.local>
2024-11-05 18:54:36 +0530

    Updates

---
+++ precache-manifest.1c06380b3b3946b1c9edf98ecbc54f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e5610a31feb49728e74d6c0369937",
+    "url": "/music-artists-react-app/index.html"
+  },
+  {
+    "revision": "141e899632b64e7a0300",
+    "url": "/music-artists-react-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a351052c1a73b7b2d985",
+    "url": "/music-artists-react-app/static/js/2.0183b995.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/music-artists-react-app/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "141e899632b64e7a0300",
+    "url": "/music-artists-react-app/static/js/main.87ccb562.chunk.js"
+  },
+  {
+    "revision": "f53677cd323dd29952d4",
+    "url": "/music-artists-react-app/static/js/runtime~main.3d1c6cb2.js"
+  }
+]);
