9eafe9edb90a523d6cccc7870a15838dd6ccefef
Prayag Kasture <prayagkasture@Prayags-MacBook-Air.local>
2024-11-05 18:47:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios"; // uimport axios
+class App extends React.Component {
+    // Constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+          location: "",
+          items: [],
+            DataisLoaded: false,
+        };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        axios.get(
+          "https://get.geojs.io/v1/ip/geo.json"
+        )
+        .then(res => {
+          this.State({
+            location: res.data.city
+          })
+        });
+        axios.get(
+                "http://musicbrainz.org/ws/2/artist/?query=begin:[2014 TO 2024] AND area:"+this.state.location
+            )
+            .then((res) => {
+                this.setState({
+                    items: res.data,
+                    DataisLoaded: true,
+                });
+            });
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded)
+            return (
+                <div>
+                    <h1> Pleses wait some time.... </h1>
+                </div>
+            );
+
+        return (
+            <div className="App">
+                <h1 className="artists">Fetch artists from last 10 years</h1>
+                <h3>Fetch data from an api in react</h3>
+                <div className="container">
+                    {items.artists.map((item) => (
+                        <div className="item">
+                            <ol key={item.id}>
+                                <div>
+                                    <strong>
+                                        {"Artist: "}
+                                    </strong>
+                                    {item.name},
+                                </div>
+                                <div>
+                                    Started in: {item['life-span'].begin}
+                                </div>
+                            </ol>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
